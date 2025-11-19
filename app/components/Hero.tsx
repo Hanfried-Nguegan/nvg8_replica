8f9c9f7b219cc4770cd17fbd1f02e739be52d5b9
Hanfried Nguegan <hanfriednguegan@Hanfrieds-MacBook-Air.local>
2025-11-19 17:43:11 +0100

    fixing ci.yml linting

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
-type Props = {}
-
-export default function Hero({}: Props) {
+export default function Hero() {
   return (
     <div>Hero</div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+//import Image from "next/image";
 import{ GsapTest }from "./components/GsapTest"
 
 export default function Home() {
